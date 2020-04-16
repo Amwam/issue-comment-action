b15915771727e1868d742324209ae08840f63193
Amit Shah <amit@amwam.me>
2020-04-16 18:13:58 +0100

    Add tests

--- __tests__/checkKeyword.test.ts
+++ __tests__/checkKeyword.test.ts
@@ -1,62 +1,22 @@
 import { checkKeyword } from "../src/checkKeyword";
 
 describe("checkKeyword", () => {
-  it("returns true if keyword is included in the title", () => {
-    const result = checkKeyword(["test"], {
-      title: "test",
-      body: "",
-      comments: [],
-    });
-    expect(result).toBe(true);
-  });
-  it("returns true if keyword is included in the body", () => {
-    const result = checkKeyword(["test"], {
-      title: "",
-      body: "test",
-      comments: [],
-    });
-    expect(result).toBe(true);
-  });
-  it("returns true if second keyword is included in title or body", () => {
-    const result = checkKeyword(["test", "bar"], {
-      title: "bar",
-      body: "",
-      comments: [],
-    });
-    expect(result).toBe(true);
-  });
-  it("returns true for different casings in keyword", () => {
-    const result = checkKeyword(["test", "Bar"], {
-      title: "bar",
-      body: "",
-      comments: [],
-    });
-    expect(result).toBe(true);
-  });
-  it("returns true for different casings in content", () => {
-    const result = checkKeyword(["test", "bar"], {
-      title: "Bar",
-      body: "",
-      comments: [],
-    });
+  it("returns true if keyword is included in the comment", () => {
+    const result = checkKeyword(["test"], "this is a test");
     expect(result).toBe(true);
   });
 
-  it("returns false if keyword is not included in title or body", () => {
-    const result = checkKeyword(["test"], {
-      title: "",
-      body: "",
-      comments: [],
-    });
+  it("returns false if keyword is not included in the comment", () => {
+    const result = checkKeyword(["test"], "this is a");
     expect(result).toBe(false);
   });
 
-  it("returns true if keyword is included in the comments", () => {
-    const result = checkKeyword(["test"], {
-      title: "",
-      body: "",
-      comments: ["other", "Test"],
-    });
+  it("returns true if any keyword is included in the comment", () => {
+    const result = checkKeyword(["other", "test"], "this is a test");
     expect(result).toBe(true);
+  });
+  it("returns false if no keyword is not included in the comment", () => {
+    const result = checkKeyword(["test", "other"], "this is a");
+    expect(result).toBe(false);
   });
 });
